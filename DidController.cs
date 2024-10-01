50ee9a92c38b08f447281278204785b73d5168a6
Darragh O'Flaherty <doflaherty@microsoft.com>
2024-10-01 11:37:19 +0100

    add did-configuration.json

--- DidController.cs
+++ DidController.cs
@@ -4,15 +4,23 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCoreVerifiableCredentials
 {
-    [Route(".well-known/did.json")]
+    [Route(".well-known")]
     public class DidController : Controller
     {
 
         [AllowAnonymous]
-        [HttpGet]
+        [HttpGet("did.json")]
         public async Task<ActionResult> GetDid()
         {
             string readText = System.IO.File.ReadAllText("./Resources/did.json");
+            return new ContentResult { ContentType = "application/json", Content = readText };
+        }
+
+        [AllowAnonymous]
+        [HttpGet("did-configuration.json")]
+        public async Task<ActionResult> GetDidConfiguration()
+        {
+            string readText = System.IO.File.ReadAllText("./Resources/did-configuration.json");
             return new ContentResult { ContentType = "application/json", Content = readText };
         }
     }
