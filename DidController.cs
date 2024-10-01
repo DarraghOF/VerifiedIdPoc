5a11fc4802d58abbe6c3efafa749543a75e8f07e
Darragh O'Flaherty <doflaherty@microsoft.com>
2024-10-01 11:26:27 +0100

    Controller test

---
+++ DidController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreVerifiableCredentials
+{
+    [Route(".well-known/did.json")]
+    public class DidController : Controller
+    {
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult> GetDid()
+        {
+            string readText = System.IO.File.ReadAllText("./Resources/did.json");
+            return new ContentResult { ContentType = "application/json", Content = readText };
+        }
+    }
+}
